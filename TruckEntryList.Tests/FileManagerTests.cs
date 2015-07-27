1435dfa5fdc6316018a601977905a7a181ec2c5a
Dima Robert <lord.specter@hotmail.com>
2015-07-28 02:03:49 +0300

    Updated Tests
    random add test

--- TruckEntryList.Tests/FileManagerTests.cs
+++ TruckEntryList.Tests/FileManagerTests.cs
@@ -18,22 +18,44 @@ namespace TruckEntryList.Tests {
 
         [TestMethod]
         public void FixedObjectFileStreamAddTest() {
-            TruckInfo ti = new TruckInfo();
-            ti.nrCrt = 5976868;
-            ti.nrAuto = "b-984-cpq";
-            ti.payload = "shit";
-            ti.dateEntry = RandomDayFunc()();
-            ti.dateRegistered = new DateTime(1876, 7, 22, 23, 41, 58);
+
+            var rcf = RandomCharFunc();
+            var rcfN = RandomCharFunc(true);
+            var rdf = RandomDayFunc();
+            var rnf = RandomNumberFunc(10, 100);
+
+            int rnd = rnf();
+
+            TruckInfo[] tis = new TruckInfo[rnd];
+            for (int i = 0; i < tis.Length; i++) {
+                TruckInfo ti = tis[i];
+                ti = new TruckInfo();
+                ti.nrCrt = RandomNumberFunc(0)();
+                ti.nrAuto = rcf().ToString() + rcf() + "-" + rcfN().ToString() + rcfN() + rcfN().ToString() + "-" + rcf().ToString() + rcf() + rcf().ToString();
+                ti.payload = "" + rcf().ToString() + rcf() + rcf().ToString() + rcf() + rcf().ToString() + rcf() + rcf().ToString() + rcf() + rcf().ToString();
+                ti.dateEntry = rdf();
+                ti.dateRegistered = rdf();
+                tis[i] = ti;
+            }
 
             Stream ms = new MemoryStream();
             FixedObjectFileStream instance = new FixedObjectFileStream(ms);
-            instance.Add(ti);
+            instance.Add(tis);
             instance.FillTestStream(ms);
 
-            ms.Seek(4, SeekOrigin.Begin);
-            TruckInfo result = new TruckInfo(ms);
+            ms.Seek(0, SeekOrigin.Begin);
+            int count;
+            byte[] b = new byte[4];
+            ms.Read(b, 0, 4);
+            count = BitConverter.ToInt32(b, 0);
+            Assert.AreEqual(count, tis.Length);
 
-            Assert.IsTrue(ti == result);
+            foreach (TruckInfo ti in tis) {
+                TruckInfo result = new TruckInfo(ms);
+
+                Assert.IsTrue(ti == result);
+            }
+            
         }
 
         Func<DateTime> RandomDayFunc() {
@@ -42,5 +64,15 @@ namespace TruckEntryList.Tests {
             int range = ((TimeSpan)(DateTime.Today - start)).Days;
             return () => start.AddDays(gen.Next(range));
         }
+        Func<int> RandomNumberFunc(int min = int.MinValue, int max = int.MaxValue) {
+            Random gen = new Random((int)(new TimeSpan(DateTime.UtcNow.Ticks) - new TimeSpan(new DateTime(1970, 1, 1).Ticks)).TotalSeconds);
+            return () => gen.Next(min, max);
+        }
+        Func<char> RandomCharFunc(bool onlyNumbers = false) {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random gen = new Random((int)(new TimeSpan(DateTime.UtcNow.Ticks) - new TimeSpan(new DateTime(1970, 1, 1).Ticks)).TotalSeconds);
+            return () => onlyNumbers ? chars[gen.Next(chars.IndexOf('0'), chars.Length)] : chars[gen.Next(chars.Length)];
+        }
+
     }
 }
